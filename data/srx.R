c07ff0f50edf5ec07ddc1bca203da7a3f150affe
Miron B. Kursa <m@mbq.me>
2018-10-15 19:33:03 +0200

    Srx dataset

---
+++ data/srx.R
@@ -0,0 +1,15 @@
+# Small XOR problem data
+# documentation in R/Boruta.R
+
+stats::setNames(
+ do.call(expand.grid,rep(list(c(T,F)),5)),
+ c("A","B","N1","N2","N3")
+)->srx
+data.frame(
+ srx,
+ AoB=with(srx,A|B),
+ AnB=with(srx,A&B),
+ nA=!srx$A
+)->srx
+srx$Y<-with(srx,factor(A!=B))
+
