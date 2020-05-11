7b5ab07eea80d5e877d7fbab15712cae0ae41f82
Miron B. Kursa <m@mbq.me>
2020-05-11 19:42:20 +0200

    More tests with mock importance source

--- tests/testthat/testBasic.R
+++ tests/testthat/testBasic.R
@@ -43,17 +43,85 @@ test_that("No-Boruta input is rejected",{
  expect_error(getNonRejectedFormula(iris))
 })
 
+#Mocked importance provider that always make Rel* features confirmed, Irr* ones rejected and others tentative
+getImpMock<-function(x,y,...) 
+ sapply(names(x),function(n) 
+  if(grepl("^Rel",n)) 7 
+  else if(grepl("^Irr",n)) -7 
+  else if(grepl("^shadow",n)) 0 
+  else rnorm(1)
+ )
+MockX<-data.frame(Rel1=1:5,Rel2=1:5,Rel3=1:5,Irr1=1:5,Irr2=1:5,Irr3=1:5,Irr4=1:5,Tent1=1:5,Tent2=1:5)
+MockY<-1:5
+
 test_that("Misc errors are caught",{
- expect_silent(Boruta(Species~.,data=iris))->B
+ set.seed(777)
+ expect_silent(Boruta(MockX,MockY,getImp=getImpMock)->B)
  expect_error(plot.Boruta(B,colCode=c("green")))
  expect_error(plotImpHistory(B,colCode=c("green")))
+
+ X<-iris[,1:4]
+ names(X)[2]<-"shadow.mystery"
+ expect_error(Boruta(X,iris$Species),"Attributes with names ")
+
+ X<-iris[,1:4]
+ X[20,3]<-NA
+ expect_error(Boruta(X,iris$Species),"Cannot process NAs in input")
+
+ expect_error(Boruta(iris[,-5],iris$Species,maxRuns=10),"maxRuns must be greater ")
 })
 
 test_that("Models without history are handled correctly",{
- Boruta(Species~.,data=iris,holdHistory=FALSE)->B
+ set.seed(777)
+ Boruta(MockX,MockY,holdHistory=FALSE,getImp=getImpMock)->B
  expect_null(B$ImpHistory)
- expect_output(print(B)," 4 attributes confirmed important")
+ expect_output(print(B)," 3 attributes confirmed important")
  expect_error(plot.Boruta(B))
  expect_error(plotImpHistory(B))
  expect_error(attStats(B))
+ expect_error(TentativeRoughFix(B))
 })
+
+test_that("TentativeRoughFix works",{
+ set.seed(777)
+ Boruta(MockX,MockY,getImp=getImpMock)->B
+ expect_error(TentativeRoughFix(B,averageOver="a"))
+ expect_error(TentativeRoughFix(B,averageOver=-11))
+ expect_error(TentativeRoughFix(B,averageOver=1:10))
+ TentativeRoughFix(B)->Bf
+ expect_equal(getSelectedAttributes(Bf),getSelectedAttributes(Bf,withTentative=TRUE))
+ expect_output(print(Bf),"Tentatives roughfixed")
+
+ set.seed(777)
+ Boruta(MockX[,1:7],MockY,getImp=getImpMock)->B
+ expect_warning(TentativeRoughFix(B))
+})
+
+test_that("Edge cases are printed",{
+ expect_message(Boruta(MockX[,1:7],MockY,getImp=getImpMock,doTrace=20),"no more attributes")
+ Boruta(MockX[,1:3],MockY,getImp=getImpMock,doTrace=20)->Bc
+ expect_output(print(Bc),"No attributes deemed unimportant")
+ Boruta(MockX[,4:7],MockY,getImp=getImpMock,doTrace=20)->Br
+ expect_output(print(Br),"No attributes deemed important")
+})
+
+test_that("Formulae are built",{
+ set.seed(777)
+ expect_silent(Boruta(MockX,MockY,getImp=getImpMock)->B)
+ expect_error(getConfirmedFormula(B))
+ expect_error(getNonRejectedFormula(B))
+
+ MockX$Y<-MockY
+ expect_silent(Boruta(Y~.,data=MockX,getImp=getImpMock)->B)
+ expect_equal(deparse(getConfirmedFormula(B)),"Y ~ Rel1 + Rel2 + Rel3")
+ expect_equal(deparse(getNonRejectedFormula(B)),"Y ~ Rel1 + Rel2 + Rel3 + Tent1 + Tent2")
+})
+
+test_that("Matrices are coerced",{
+ expect_silent(Boruta(as.matrix(MockX),MockY,getImp=getImpMock)->B)
+ expect_equal(getSelectedAttributes(B),c("Rel1","Rel2","Rel3"))
+ expect_equal(getSelectedAttributes(B,TRUE),c("Rel1","Rel2","Rel3","Tent1","Tent2"))
+})
+
+
+
--- tests/testthat/testSources.R
+++ tests/testthat/testSources.R
@@ -1,5 +1,7 @@
 context("Importance source tests")
 
+#Regular test
+
 data(srx)
 X<-srx[,-ncol(srx)]
 Y<-srx$Y
@@ -34,4 +36,25 @@ test_that("Invalid source is caught",{
  expect_error(Boruta(Species~.,data=iris,getImp=function(...) 1:10),"getImp result has a wrong length")
  expect_error(Boruta(Species~.,data=iris,getImp=function(...) "x"),"getImp result is not a numeric vector")
  expect_warning(Boruta(Species~.,data=iris,getImp=function(x,...) c(0,rep(NA,ncol(x)-1))),"getImp result contains NA")
+})
+
+
+#Censored data test
+
+X<-iris[,2:5]
+X$N1<-runif(150)
+X$N2<-runif(150)
+Yt<-iris[,1]
+Ys<-Yt<=6.9
+Yt[Yt>6.9]<-6.9
+
+#Mock Surv object
+Y<-cbind(time=Yt,status=Ys)
+class(Y)<-c("Surv")
+
+test_that("Ranger sources work on censored",{
+ set.seed(777)
+ expect_error(Boruta(X,Y,getImp=getImpRfGini),"Ranger cannot produce Gini")
+ expect_equal(sort(getSelectedAttributes(Boruta(X,Y))),sort(names(iris)[-1]))
+ expect_equal(sort(getSelectedAttributes(Boruta(X,Y,getImp=getImpRfRaw))),sort(names(iris)[-1]))
 })
